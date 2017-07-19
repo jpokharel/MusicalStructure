60870ea5cedbbe4db0d5d658f2993c26120269a5
jiwanpokharel89 <jpokharel@luc.edu>
2017-07-19 00:09:05 -0500

    Added additional layouts and activities. Deals with FM Radios, Browse, Search and adding additional intents to move within activities.

--- app/src/main/java/com/example/android/musicalstructure/MusicGalleryActivity.java
+++ app/src/main/java/com/example/android/musicalstructure/MusicGalleryActivity.java
@@ -1,13 +1,11 @@
 package com.example.android.musicalstructure;
 
 import android.content.Intent;
-import android.net.Uri;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
-
-import java.io.File;
 
 public class MusicGalleryActivity extends AppCompatActivity {
 
@@ -17,15 +15,49 @@ public class MusicGalleryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_music_gallery);
 
         ImageView music_gallery_img = (ImageView) findViewById(R.id.library);
+        ImageView store_img = (ImageView) findViewById(R.id.store);
+        ImageView browse_img = (ImageView) findViewById(R.id.browse);
+        ImageView radio_img = (ImageView) findViewById(R.id.radio);
+        ImageView search_img = (ImageView) findViewById(R.id.search);
 
         music_gallery_img.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData("C:\\Users\\jiwanpokharel89\\AndroidStudioProjects\\MusicalStructure\\app\\src\\main\\res\\raw\\music1.mp3");
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    startActivity(intent);
-                }
+                MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.music1);
+                mediaPlayer.start();
+//                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                intent.setData("C:\\Users\\jiwanpokharel89\\AndroidStudioProjects\\MusicalStructure\\app\\src\\main\\res\\raw\\music1.mp3");
+//                if (intent.resolveActivity(getPackageManager()) != null) {
+//                    startActivity(intent);
+//                }
 
+            }
+        });
+
+        store_img.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(MusicGalleryActivity.this, StoreActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        browse_img.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(MusicGalleryActivity.this, BrowseActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        radio_img.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(MusicGalleryActivity.this, FmRadioActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        search_img.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(MusicGalleryActivity.this, SearchActivity.class);
+                startActivity(intent);
             }
         });
     }
